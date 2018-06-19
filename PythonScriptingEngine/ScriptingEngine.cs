c641f271263f18c8c7b54838e559fc1d7ab4beae
Andrew Yasman <ajyasman@gmail.com>
2018-06-18 18:18:51 -0600

    Initial commit.

---
+++ PythonScriptingEngine/ScriptingEngine.cs
@@ -0,0 +1,19 @@
+﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using System;
+using System.Collections.Generic;
+
+namespace PythonScriptingEngine
+{
+    public class ScriptingEngine
+    {
+        private Dictionary<string, Tuple<ScriptSource, CompiledCode>> scriptFiles = new Dictionary<string, Tuple<ScriptSource, CompiledCode>>();
+        
+        private ScriptEngine pythonEngine;
+
+        public void Initialize()
+        {
+            pythonEngine = Python.CreateEngine();
+        }
+    }
+}
---
+++ PythonScriptingEngine/TestScripts/TestScript.py
@@ -0,0 +1,23 @@
+﻿import clr
+import System
+clr.AddReference("System.Core")
+clr.AddReference("System.Windows.Forms")
+clr.ImportExtensions(System.Linq)
+from System import String
+from System import Guid
+from TestIronPython.Scripting.Interfaces import IActionScript
+
+class Script1(IActionScript):
+    def Load(this):
+        print "1"
+    def Unload(this):
+        print "2"
+    def Execute(this):
+        print "3"
+    def CanExecute():
+        return true
+
+pluginID = Guid.Parse("378F108E-A65A-4599-89EC-26F99872B8FB")
+relatedModuleID = Guid.Parse("407A91ED-5937-4964-9B26-10E3D17D26DB")
+plugin = Script1()
+ScriptingManager.RegisterScript(pluginID, relatedModuleID, plugin)
